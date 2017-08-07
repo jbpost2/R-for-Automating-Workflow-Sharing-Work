4ce0a29eab3d8edbab13acba1a6a1b373c748645
Justin Post <jbpost2@ncsu.edu>
2017-08-06 20:08:41 -0400

    Add files via upload

---
+++ files/BasicUI/ui.r
@@ -0,0 +1,19 @@
+library(ggplot2)
+
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel(" "),
+  
+  # Sidebar with options for the data set
+  sidebarLayout(
+    sidebarPanel(
+      
+    ),
+    
+    # Outputs  
+    mainPanel(
+     
+      )
+  )
+))
